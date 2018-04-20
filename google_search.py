aa50ebd653a42275c62feee0834fe24a0915c5d6
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-04-21 01:59:26 +0600

    Automatic google search using python and selenium

---
+++ google_search.py
@@ -0,0 +1,18 @@
+import time
+from random import randint
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://www.google.com/")
+assert "Google" in driver.title
+t = randint(10,15)
+time.sleep(t)
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("python")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+t = randint(10,15)
+time.sleep(t)
+driver.close()
