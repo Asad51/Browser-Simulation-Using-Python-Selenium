9adb9117f57dbb0c2478d2d23fee73e1f81188d0
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-05-17 00:49:05 +0600

    Modified

--- AutoEmailSender_Gmail.py
+++ AutoEmailSender_Gmail.py
@@ -7,22 +7,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-#Important declarations
-emails = ["asad.cse.ru.15@gmail.com", "ashiqur.cse.ru@gmail.com", "asad.cse@yahoo.com"]
-
 #opening browser
 driver = webdriver.Chrome()
 driver.get("http://www.gmail.com/")
 assert "Gmail" in driver.title
 time.sleep(5)
 
+##User Details
+userEmail = "you@example.com"
+userPwd = "password"
+
 #enter email address
-driver.find_element_by_name("identifier").send_keys("rifata441@gmail.com")
+driver.find_element_by_name("identifier").send_keys(userEmail)
 driver.find_element_by_id("identifierNext").click()
 time.sleep(5)
 
 #enter password
-driver.find_element_by_name("password").send_keys("asadulsharmin")
+driver.find_element_by_name("password").send_keys(userPwd)
 driver.find_element_by_id("passwordNext").click()
 time.sleep(20)
 
@@ -43,9 +44,9 @@ time.sleep(5)
 
 #Message Body
 driver.find_element_by_css_selector(".Ap .Au div").send_keys("Hey, what's up?")
-#.send_keys(Keys.CONTROL + Keys.RETURN)
 time.sleep(5)
 
 driver.find_element_by_css_selector(".btC .Up .btA .aoO").click()
+time.sleep(30)
 
 driver.close();
